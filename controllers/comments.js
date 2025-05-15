f389795f7780fcd3c3e5220afbc3ee9e6f3f5a90
Manoj <mkv336699@gmail.com>
2025-05-15 22:17:58 +0530

    Populate and Sort comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,7 +4,7 @@ const handleGetAllComments = async (req, res) => {
     const id = req.params.id;
     if (!id) return res.json({ error: "Invalid id" });
 
-    const comments = await Comment.find({ commentedAt: id });
+    const comments = await Comment.find({ commentedAt: id }).populate("createdBy", "name profileImageURL").sort({createdAt: -1});
     res.json({ data: comments });
 }
 
